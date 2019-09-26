af944ae54b6a15556befa3c953fea57c68d5e53c
jayfr-c <jfcomparativo@up.edu.ph>
2019-09-26 10:21:15 +0800

    Admin page update

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-$(document).ready(function(){
+ console.log("check")
+ $(document).ready(function(){
     $('.header').height($(window).height());
   })
 $(document).ready(function(){
@@ -8,30 +9,10 @@ $(document).ready(function(){
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });
-});
-  function adminlog() {
-    location.replace("adminlog.html")
-  }
-  function homepage() {
-    location.replace("admin.html")
-  }
-  function studentcent(){
-    location.replace("studentcent.html")
-  }
-  jQuery(document).ready(function($) {
-    $(".clickable-row").click(function() {
-        window.location = $(this).data("href");
-    });
-});
-  $(document).ready(function () {
-    $('#dtBasicExample').DataTable({
-      "paging": false // false to disable pagination (or any other option)
-    });
-    $('.dataTables_length').addClass('bs-select');
+});   
+$(document).ready(function($) {
+  $(".clickable-row").click(function() {
+    console.log("transferred.................");
+      window.location = $(this).data("href");
   });
-  $(document).ready(function () {
-    $('#dtBasicExample').DataTable({
-      "pagingType": "simple" // "simple" option for 'Previous' and 'Next' buttons only
-    });
-    $('.dataTables_length').addClass('bs-select');
-  });
+}); 
