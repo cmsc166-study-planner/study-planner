3bea6f2c45d50a76882764c0cff99f73bb275809
Comparativo Jayfer <jfcomparativo@up.edu.ph>
2019-10-12 15:24:56 +0800

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,113 @@
+const ref = firebase.database().ref();
+const subjectsRef = firebase.database().ref("subjects/");  
+
+
+var database = firebase.database();
+
+ 
+subjectsRef.on('value', function(snapshot) {  
+  var content = "";
+  console.log('asdf')
+  snapshot.forEach(function(childSnapshot){  
+    var value = childSnapshot.val();  
+    console.log(value); 
+  });  
+});
+/*function courseList(){        
+  var tableBody = document.getElementById("courserows");
+  dbRef.child("subjects").orderByChild("code").on("value", snapshot => {
+      if(snapshot.exists()){ 
+          snapshot.forEach(function(childSnapshot) {
+            childSnapshot.forEach(function(listSnapshot){
+                let obj = listSnapshot.val();   
+                  for(let key in obj){
+                    if(obj[key].name != undefined){  
+                      var tr = document.createElement('TR');
+                      var tdname = document.createElement('TD');
+                      var tdstatus = document.createElement('TD');
+                      tdstatus.appendChild(document.createTextNode('status'));  
+                      tdname.appendChild(document.createTextNode(obj[key].name+" ")); 
+                      tr.appendChild(tdstatus); 
+                      tr.appendChild(tdname);
+                      tableBody.appendChild(tr);
+                    } 
+                    else{
+                      console.log('undefined'+ key);
+                    } 
+                }
+              });
+          });
+      }
+      else{
+          console.log("The subject does not exist")
+      }
+  }); 
+}*/
+ 
+
+var subjectdata = "no data";
+var getTable = document.getElementById("course-table");
+var createClickHandler = function(row) {
+  return function() {
+    var cell = row.getElementsByTagName("td")[0];
+    var id = cell.innerHTML;  
+    subjectdata = id;
+    console.log("row clicked: "+ subjectdata);  
+  };
+};
+getTable.addEventListener('click', function(event){
+  console.log("clicked"); 
+  var rows = getTable.getElementsByTagName("tr"); 
+  for (i = 0; i < rows.length; i++) { 
+    var currentRow = getTable.rows[i]; 
+    
+    currentRow.onclick = createClickHandler(currentRow); 
+  }
+  console.log("data sent: "+ subjectdata);
+  localStorage.setItem("subname", subjectdata);
+  //window.open('coursestats.html');
+}) 
+/*document.addEventListener('readystatechange', event => {
+  var subname = localStorage.getItem("subname");
+  if (event.target.readyState === "interactive") {      //same as:  document.addEventListener("DOMContentLoaded"...   // same as  jQuery.ready
+      alert("All HTML DOM elements are accessible");
+  }
+
+  if (event.target.readyState === "complete") {
+      alert("Now external resources are loaded too, like css,src etc... ");
+      alert(subname);
+  }
+  studentList(subname); 
+});*/
+
+function studentList(subject){   
+  console.log('subject: ' + subject);     
+  var tableBody = document.getElementById("student-rows");
+  dbRef.child("subjects").orderByChild("code").equalTo(subject).once("value", snapshot => {
+      if(snapshot.exists()){ 
+          snapshot.forEach(function(childSnapshot) {
+              childSnapshot.forEach(function(listSnapshot){
+                  let obj = listSnapshot.val();   
+                    for(let key in obj){
+                      if(obj[key].name != undefined){  
+                        var tr = document.createElement('TR');
+                        var tdname = document.createElement('TD');
+                        var tdstatus = document.createElement('TD');
+                        tdstatus.appendChild(document.createTextNode('status'));  
+                        tdname.appendChild(document.createTextNode(obj[key].name+" ")); 
+                        tr.appendChild(tdstatus); 
+                        tr.appendChild(tdname);
+                        tableBody.appendChild(tr);
+                      } 
+                      else{
+                        console.log('undefined'+ key);
+                      } 
+                }
+              });
+          });
+      }
+      else{
+          console.log("The subject does not exist")
+      }
+  }); 
+} 
