e028e0c5e957b75beaedc2ec705658e3ca9629fa
ltcunanan <ltcunanan@up.edu.ph>
2019-11-04 02:13:11 +0800

    admin features done

---
+++ admin.js
@@ -0,0 +1,62 @@
+
+var counter = 0;
+const dbRef = firebase.database().ref();
+
+var count = 0;
+
+dbRef.child("students").once('value', function (snapshot){
+	let students = snapshot.val();
+	for(let key in students){
+		count++;
+	}
+});
+
+//list of subjects
+dbRef.child("subjects").once('value', function (subjectsnapshot){
+	let subject_obj = subjectsnapshot.val();
+	for(let key3 in subject_obj){
+		//console.log(subject_obj[key3].code);
+		if(subject_obj[key3].code != "CMSC 195"){
+		//list of all students
+			dbRef.child("students").once('value', function (snapshot){
+				let student_obj = snapshot.val();
+				for(let key in student_obj){
+					//console.log("Original student"+ student_obj[key].name);
+					//get list of students of a subject
+					dbRef.child("subjects").orderByChild("code").equalTo(subject_obj[key3].code).once("value", subjectsnapshot => {
+						if(subjectsnapshot.exists()){
+							var key1 = Object.keys(subjectsnapshot.val())[0];
+							const subjectStudentRef = dbRef.child('subjects/' + key1 +'/studentsTaken');
+							subjectStudentRef.once('value', function (subjectStudentsnapshot){
+								let student_list = subjectStudentsnapshot.val();
+								for(let key2 in student_list){
+									counter++;						
+								}
+								//console.log(counter);
+								var htmlname = subject_obj[key3].code;
+								htmlname = htmlname.replace(/\s+/g, '-');
+								//console.log(htmlname);
+
+								document.getElementById(htmlname).innerHTML = count - counter;
+								counter = 0;
+							});
+						}
+					});
+				} 
+			});
+		}
+	}
+});
+
+function studentList(course){
+	var x = document.getElementById(course).value;
+	//x undefined
+	console.log(x); //alert is still not working because x takes time to load. 
+	if(x == 0){
+		alert("All the students in the student list have taken this subject");
+	}
+	else{
+		location.href = 'list.html?course='+course;
+	}
+	//window.location = '/list.html?course='+course;
+}
---
+++ list.js
@@ -0,0 +1,45 @@
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+const dbRef = firebase.database().ref();
+var flag = 0;
+var mycourse = getUrlVars()["course"];
+//console.log(mycourse);
+mycourse = mycourse.replace(/-/g, ' ');
+document.getElementById('course_name').innerHTML = mycourse;
+
+const studentListUI = document.getElementById("studentList");
+
+dbRef.child("students").once('value', function (snapshot){
+	let student_obj = snapshot.val();
+	for(let key in student_obj){
+		//console.log("Original student"+ student_obj[key].name);
+		//get list of students of a subject
+		dbRef.child("subjects").orderByChild("code").equalTo(mycourse).once("value", subjectsnapshot => {
+			if(subjectsnapshot.exists()){
+				var key1 = Object.keys(subjectsnapshot.val())[0];
+				const subjectStudentRef = dbRef.child('subjects/' + key1 +'/studentsTaken');
+				subjectStudentRef.once('value', function (subjectStudentsnapshot){
+					let student_list = subjectStudentsnapshot.val();
+					for(let key2 in student_list){
+						if(student_obj[key].name == student_list[key2].name){
+							flag = 1;
+						}					
+					}
+					if(flag == 0){
+						let $li = document.createElement("li");
+						$li.innerHTML = student_obj[key].name;
+						studentListUI.append($li);
+						//console.log(student_obj[key].name);
+					}
+					flag = 0;
+				});
+			}
+		});
+	} 
+});
